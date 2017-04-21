f17b5c0cfddd42650ca53d20d669069178cfa6f7
leehyso <nohf2002@gmail.com>
2017-04-21 14:11:59 +0900

    Add files via upload

---
+++ Questing.java
@@ -0,0 +1,19 @@
+package nomadconnection_coding;
+/**
+ * 테스트 코드를 보고, 아래 두 method 를 작성해주세요.
+ */
+class Questing {
+	static int quest1(double d)  {
+		// FIXME
+		return (int)d / 2;
+	}
+
+	static boolean quest2(String s) {
+		// FIXME
+		String check = "({}[])";
+		if(s.equals(check))
+			return true;
+		else
+			return false;
+	}
+}
